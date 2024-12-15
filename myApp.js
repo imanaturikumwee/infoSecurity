701dac1a04b7d8345d63f08e042153dcd6169645
imanaturikumwee <imanaturikumwee@gmail.com>
2024-12-15 20:01:32 +0000

    https

--- myApp.js
+++ myApp.js
@@ -4,8 +4,10 @@ const app = express();
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({action: 'deny'}));
 app.use(helmet.xssFilter({}));
-app.use(helmet.noSniff())
-app.use(helmet.ieNoOpen())
+app.use(helmet.noSniff());
+app.use(helmet.ieNoOpen());
+const ninetyDaysInSeconds = 90*24*60*60;
+app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}));
 
 
 
